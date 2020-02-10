693dc2dbd416c3cf04d3464f86aff684bcf386d7
mnickii <caroline.nkirote@andela.com>
2020-02-10 12:50:12 +0300

    Initial Commit

---
+++ app/components.js
@@ -0,0 +1,7 @@
+angular.module('components', [])
+ 
+  .directive('notes', function() {
+    return {
+      scope: {},
+    };
+  });
---
+++ app/todo.js
@@ -0,0 +1,37 @@
+angular.module('todoApp', ['components'])
+  .controller('TodoListController', function() {
+    var todoList = this;
+    todoList.todos = [
+      {text:'learn Angularjs basics', done:true},
+      {text:'deliver simple crud app', done:true},
+      {text:'modify to more features', done:false}];
+ 
+    todoList.addTodo = function() {
+      todoList.todos.push({text:todoList.todoText, done:false});
+      todoList.todoText = '';
+    };
+ 
+    todoList.remaining = function() {
+      var count = 0;
+      angular.forEach(todoList.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+ 
+    todoList.archive = function(todo) {
+      var oldTodos = todoList.todos;
+      todoList.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) todoList.todos.push(todo);
+      });
+    };
+  })
+  .controller('NotesController', function($scope) {
+    $scope.notes = [ 
+      {title:'Angularjs basics', 
+      description: 'Learn the MVS structure and create simple CRUD app.',
+      edited:true},
+      {title:'Available features', description: 'Only basic features implemented', edited:false}];
+    
+  })
